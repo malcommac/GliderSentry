510cd68a5c3555a8432b1a9f5db0846ffd0ef65a
Daniele Margutti <hello@danielemargutti.com>
2022-06-09 10:32:22 +0200

    Initial structure

---
+++ Sources/GliderSentry/GliderSentry.swift
@@ -0,0 +1,6 @@
+public struct GliderSentry {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/GliderSentryTests/GliderSentryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import GliderSentry
+
+final class GliderSentryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(GliderSentry().text, "Hello, World!")
+    }
+}
